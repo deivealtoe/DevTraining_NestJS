18ffa245c0600d4d9d6d62751bf094d087bfabd1
Deivisson Altoé <deivealtoe@gmail.com>
2022-04-28 00:01:43 -0300

    Adicionado parâmetros no endpoint

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
-  findAll() {
-    return `Hello, World from Courses!`;
+  listAll() {
+    return `Listagem de cursos completa!`;
+  }
+
+  @Get(':id')
+  findOne(@Param() params) {
+    return `Curso número: ${params.id}`;
+  }
+
+  @Get('/outros/:id')
+  findOutros(@Param('id') id: string) {
+    return `Curso #${id}`;
   }
 }
